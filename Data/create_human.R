d6afebc9d0e5fe3757dd61432930b6b8d8b805d3
timothyzwz <33382119+timothyzwz@users.noreply.github.com>
2017-12-12 23:06:03 +0200

    Data2

--- Data/create_human.R
+++ Data/create_human.R
@@ -1,11 +1,14 @@
 ## Wenzhong Zhang
+## Email: Wenzhong.Zhang@helsinki.fi
 ## 11th November 2017
 ## Data wrangling for final project
+## The Human Developement (HD) and Gender Inequality Index (GII) data were joined by the country name, the incomplete data filtered out, and the ratio of education completeness added as a new column. The level of hdi was categorized into four degrees (low, medium, high and very high) according to the description. Some unnecessary data (related to the hypothesis) was removed from the data. 
 
-# access the dplyr, tidyr and ggplot2 library
+# access the dplyr, tidyr, ggplot2 and stringr library
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(stringr)
 
 # read the dataset
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -60,6 +63,26 @@ human_ <- filter(human, complete.cases(human) == TRUE)
 # define a new column representing the ratio of the mean completeness of education
 human_ <- mutate(human_, meaneduratio = meanedu / edutime)
 
+# define a new logical column 'teenbirth_high'
+human_ <- mutate(human_, teenbirth_high = teenbirth > 33)
+
+# remove the commas from gni
+human_ <- within(human_, {
+  gni <- as.character(gni)
+})
+human_ <- within(human_, {
+  gni <- as.numeric(sub(",", "", gni, fixed = TRUE))
+})
+
+# create a vector to separate different degrees of human development according to definition
+sep <- c(0.348, 0.55, 0.699, 0.799, 0.944)
+
+# create a categorical variable 'crime'
+hdi_d <- cut(human_$hdi, breaks = sep, include.lowest = TRUE, labels = c("low", "med", "high", "very_high"))
+
+# add the new categorical value to scaled data
+human_ <- data.frame(human_, hdi_d)
+
 # add countries as rownames
 rownames(human_) <- human_$cty
 
@@ -70,8 +93,7 @@ human_ <- dplyr::select(human_, -cty)
 str(human_)
 dim(human_)
 glimpse(human_)
-
-human_
+summary(human_)
 
 # Saving the analysis daatset, name it as human.txt, under the data folder
 write.table(human_, file = "Data/human.txt")
